25716d499e46d87f40b4f38c180e1224e533d4ef
jayawickrama <nishantharaka2004@gmail.com>
2024-12-12 00:02:10 +0530

    mappingClass

--- src/main/java/com/example/trainticketbookingsystem/Util/Mapping.java
+++ src/main/java/com/example/trainticketbookingsystem/Util/Mapping.java
@@ -1,4 +1,11 @@
 package com.example.trainticketbookingsystem.Util;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
 }
