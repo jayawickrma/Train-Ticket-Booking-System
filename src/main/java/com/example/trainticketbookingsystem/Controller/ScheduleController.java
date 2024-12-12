44d3ee781d1d1637d27ae64ffab40a7350f431c9
jayawickrama <nishantharaka2004@gmail.com>
2024-12-12 20:23:15 +0530

    ScheduleController.saveSchedule completly done

--- src/main/java/com/example/trainticketbookingsystem/Controller/ScheduleController.java
+++ src/main/java/com/example/trainticketbookingsystem/Controller/ScheduleController.java
@@ -1,4 +1,36 @@
 package com.example.trainticketbookingsystem.Controller;
 
+import com.example.trainticketbookingsystem.DTO.IMPL.ScheduleDTO;
+import com.example.trainticketbookingsystem.Exception.DataPersistException;
+import com.example.trainticketbookingsystem.Service.ScheduleService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/trainBooking/schedule")
 public class ScheduleController {
+    @Autowired
+    private ScheduleService scheduleService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void>saveSchedule(@RequestBody ScheduleDTO scheduleDTO){
+        try{
+            scheduleService.saveSchedule(scheduleDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
