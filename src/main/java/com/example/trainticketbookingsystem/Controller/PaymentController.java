a61e296d2b559b79016dcd0edc77eef7229676bd
jayawickrama <nishantharaka2004@gmail.com>
2024-12-12 15:52:36 +0530

    PaymentController.savePayment done

--- src/main/java/com/example/trainticketbookingsystem/Controller/PaymentController.java
+++ src/main/java/com/example/trainticketbookingsystem/Controller/PaymentController.java
@@ -1,4 +1,32 @@
 package com.example.trainticketbookingsystem.Controller;
 
+import com.example.trainticketbookingsystem.DTO.IMPL.PaymentDTO;
+import com.example.trainticketbookingsystem.Exception.DataPersistException;
+import com.example.trainticketbookingsystem.Service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/trainBooking/payment")
 public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+    @PostMapping
+    public ResponseEntity<Void>savePayment(@RequestBody PaymentDTO paymentDTO) {
+        try {
+            paymentService.savePayment(paymentDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
