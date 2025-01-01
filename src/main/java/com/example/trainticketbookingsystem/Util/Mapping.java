f6a1195b17df6d79377f759c74968d3605f16c62
jayawickrama <nishantharaka2004@gmail.com>
2025-01-01 09:56:04 +0530

    almost done

--- src/main/java/com/example/trainticketbookingsystem/Util/Mapping.java
+++ src/main/java/com/example/trainticketbookingsystem/Util/Mapping.java
@@ -7,7 +7,7 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+    import java.util.List;
 import java.util.Optional;
 
 @Component
